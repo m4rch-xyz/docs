94ef8ec3841e7907176b51efe501b12d64e9be56
m4rch3n1ng <63159454+m4rch3n1ng@users.noreply.github.com>
2021-02-07 18:32:42 +0100

    fixed spacing issue, slight adjustments to ui, added changelog

--- load.js
+++ load.js
@@ -1,20 +1,40 @@
 window.onload = function () {
+	/* - CODE - */
+	// main
 	$("pre.hljs").on("click", el => copy(el.target))
-	$("#action-bar-close").on("click", ({ target }) => $(target).parent().css("display", "none"))
 
+	// extend
 	$(".extend-code").on("click", el => toggleCode(el.target))
 	$(".extend-code").css("transition", "1s")
 
+
+	/* - ACTION-BAR - */
+	$("#action-bar-close").on("click", ({ target }) => $(target).parent().css("display", "none"))
+
+
+	/* - CONTENT - */
 	$(".sub-content-content").css("transition", "border 0.4s")
-	$(".sub-content-content a").css("transition", "color 0.4s")
+
+
+	/* - NAVIGATOR - */
+	// main
 	$("#navigator-heading").css("transition", "0.4s")
 	$(".navigator-top").css("transition", "0.4s")
 
+	// extend
 	$("#extend-navigator").on("click", () => toggleNavigator("on"))
 	$("#navigator-overlay").on("click", () => toggleNavigator("off"))
 
-	$(".project").css("transition", "0.4s")
 
+	/* - LIGHTDARK - */
 	$("#lightdark").on("click", toggleLightDark)
 	$("#lightdark").css("transition", "0.4s")
+
+
+	/* - LINK - */
+	$("a").css("transition", "color 0.4s")
+
+
+	/* - PROJECT - */
+	$(".project").css("transition", "0.4s")
 }
